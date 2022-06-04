24c48742c15e6761285f27926a93fa700828777f
DavsProg <106853804+DavsProg@users.noreply.github.com>
2022-06-04 00:00:20 -0500

    Create PrimerSwitch.java

---
+++ PrimerSwitch.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        var estacion = "Invierno";
+
+        switch (estacion){
+            case "verano":
+                System.out.println("Es verano");
+                break;
+            case "otono":
+                System.out.println("Es verano");
+                break;
+            case "primavera":
+                System.out.println("Es verano");
+                break;
+            case "Invierno":
+                System.out.println("Es verano");
+                break;
+        }
+
+    }
